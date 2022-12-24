f034e0f9c1f15fac6712b7556a367e6f541eaf47
Daniel T. McGuiness <101267293+dTmC0945@users.noreply.github.com>
2022-12-24 22:06:27 +0100

    Add files via upload

---
+++ variables.rs
@@ -0,0 +1,11 @@
+// Code that prints out the integer using print! and println!
+
+fn main(){
+    let age = 3;
+
+    // print using println!
+    println!("{}", age);
+
+    // print using print!
+    print!("{}", age);
+}
